8c7e418f235fd8ea403c5d646b6eca2b9fa367e9
decknho <decknho26@gmail.com>
2026-05-21 05:27:11 -0300

    automação

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,9 +1,38 @@
 import pandas as pd
-from utils import carregar_csv
+from utils import *
 
-caminho = '../data/raw/telecon.csv'
-df = carregar_csv(caminho)
 
-df.head()
-def tratar_nulos():
-    return
+def tratar_nulos(df, coluna, funcao):
+    if funcao == 1:
+        return apagar_linhas(df=df, coluna=coluna)
+    elif funcao == 2:
+        return apagar_coluna(df=df, coluna=coluna)
+    elif funcao == 3:
+        return preencher_media(df=df, coluna=coluna)
+    elif funcao == 4:
+        return preencher_mediana(df=df, coluna=coluna)
+    elif funcao == 5:
+        return preencher_moda(df=df, coluna=coluna)
+
+
+def apagar_linhas(df, coluna):
+    return df.dropna(subset=[coluna], inplace=True)
+
+
+def apagar_coluna(df, coluna):
+    return df.drop(columns=[coluna], inplace=True)
+
+
+def preencher_media(df, coluna):
+    df[coluna] = df[coluna].fillna(df[coluna].mean())
+    return df
+
+
+def preencher_mediana(df, coluna):
+    df[coluna] = df[coluna].fillna(df[coluna].median())
+    return df
+
+
+def preencher_moda(df, coluna):
+    df[coluna] = df[coluna].fillna(df[coluna].mode()[0])
+    return df
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,76 @@
 import pandas as pd
 
+def configurar_pandas():
+    return
+
+def configurar_seaborn():
+    return
+
+
+def salvar_grafico(nome):
+    return
+
+
 def carregar_csv(caminho, sep=';'):
     df = pd.read_csv(caminho, delimiter=sep)
     return df
 
+
+def mostrar_percentual_nulos(df):
+    print('Serão mostrados apenas as porcetagens de colunas com dados faltantes:')
+
+    total_nulos = df.isnull().sum().sum()
+
+    if total_nulos == 0:
+        print('Não existe nenhuma coluna com dados faltantes.')
+        return
+
+    for coluna in df.columns:
+
+        porcentagem = (df[coluna].isnull().sum() / len(df)) * 100
+
+        if porcentagem == 0:
+            continue
+        
+        print(f'\nColuna: {coluna} | Dados faltantes: {porcentagem:.2f}%\n')
+        
+        if porcentagem <= 5:
+            print('- Pouco impacto. Pode remover linhas ou '
+                  'preencher com média, mediana ou moda.')
+
+        elif porcentagem <= 20:
+            print('- Dependendo do contexto, pode ser melhor '
+                  'imputar ou considerar remover a coluna.')
+
+        elif porcentagem <= 40:
+            print('- Remover linhas pode ser arriscado. '
+                  'Considere imputações mais robustas:\n'
+                  '  - média/mediana\n'
+                  '  - KNN\n'
+                  '  - regressão')
+
+        else:
+            print('- A coluna possui muitos dados faltantes.\n'
+                  'Considere remover a coluna.\n'
+                  'Caso a variável seja booleana, dependendo do contexto, '
+                  'os valores nulos podem representar False.')
+
+
+def renomear_colunas(df):
+    ...
+
+
+def converter_datas(df, coluna):
+    ...
+
+
+def verificar_dataframe(df):
+    return df.head()
+
+
+def memoria_dataframe(df):
+    ...
+
+
+def separar_features_target(df, target):
+    ...
